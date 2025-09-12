164d4ccde62d68a74dcfb7ed745ab1fc9284cded
Ömer Apaydın <omerapaydin90@gmail.com>
2025-09-13 00:53:19 +0300

    Identity Services

--- Program.cs
+++ Program.cs
@@ -1,9 +1,23 @@
 using AppleStore.Data.Abstract;
 using AppleStore.Data.Concrete.EfCore;
+using AppleStore.Entity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.Configure<IdentityOptions>(options=>{
+    options.Password.RequireLowercase = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;  
+    options.User.RequireUniqueEmail = true;  
+    // options.SignIn.RequireConfirmedEmail = true;
+});
+
+builder.Services.ConfigureApplicationCookie(options =>{
+    options.LoginPath = "/Account/Login"; 
+});
 
 
 builder.Services.AddControllersWithViews();
@@ -18,10 +32,14 @@ builder.Services.AddScoped<IProductRepository, EfProductRepository>();
 builder.Services.AddScoped<ICategoryRepository, EfCategoryRepository>();
 
 
+builder.Services.AddIdentity<ApplicationUser,IdentityRole>().AddEntityFrameworkStores<IdentityContext>().AddDefaultTokenProviders();
+
 var app = builder.Build();
 
 app.UseStaticFiles();
 app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
 app.UseHttpsRedirection();
 
 app.MapControllerRoute(
