921a30d5c86b30f10383d784b5337a01b6519e66
Ömer Apaydın <omerapaydin90@gmail.com>
2025-09-25 01:40:55 +0300

    ProcessPayment

--- obj/Debug/net9.0/AppleStore.AssemblyInfo.cs
+++ obj/Debug/net9.0/AppleStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a7e57023d8d833e14d93ec5d42f14c32a6c859")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7aaa09dacf1a398386dafdcb20beb22006991fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
