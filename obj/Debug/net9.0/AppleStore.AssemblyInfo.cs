3a626c8689249ca0eb9ec832b09ded2117280806
Ömer Apaydın <omerapaydin90@gmail.com>
2025-10-17 01:31:12 +0300

    Düzenleme

--- obj/Debug/net9.0/AppleStore.AssemblyInfo.cs
+++ obj/Debug/net9.0/AppleStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2012eafd063afec08da9472c714f5b5c5c206149")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d37452e04994439bbccd7467e0e2bc4c07d5c36c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppleStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
